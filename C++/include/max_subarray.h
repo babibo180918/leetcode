d2e4eb72a04daa611fb2c02b779a349207640c9b
babibo <nguyendtnhan@gmail.com>
2019-12-28 13:19:04 +0900

    - Solution for max subarray problem

---
+++ C++/include/max_subarray.h
@@ -0,0 +1,21 @@
+#include <vector>
+
+class Solution_max_subarray
+{
+public:
+	int maxSubArray(vector<int> &nums)
+	{
+		int max = nums[0];
+		int sum = nums[0];
+		for(int i=1; i<nums.size(); i++)
+		{
+			if(sum < 0)
+				sum = nums[i];
+			else
+				sum += nums[i];
+			if(sum>max)
+				max = sum;
+		}
+		return max;
+	}	
+}
