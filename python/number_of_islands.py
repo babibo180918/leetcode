9a6d18cbb12cf1b5f84f9352b0be701d20e6f874
Phuong Do <dongphuong2004@gmail.com>
2020-03-07 02:54:27 +0700

    Extract union-find into a new class

--- python/number_of_islands.py
+++ python/number_of_islands.py
@@ -1,31 +1,7 @@
-class Solution(object):
-    ids = []
+class UnionFind(object):
 
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
-        total = 0
-        union_count = 0
-        rows = len(grid)
-        if rows == 0: return 0
-        cols = len(grid[0])
-        if cols == 0: return 0
-        self.ids = [-1 for i in range(cols * rows)]
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == '1':
-                    total += 1
-                    cur_root = self.root(i * cols + j)
-                    if j < cols - 1 and grid[i][j + 1] == '1':
-                        if self.union(i * cols + j, i * cols + j + 1):
-                            union_count += 1
-                    if i < rows - 1 and grid[i + 1][j] == '1':
-                        if self.union(i * cols + j, (i + 1) * cols + j):
-                            union_count += 1
-
-        return total - union_count
+    def __init__(self, size):
+        self.ids = [-1 for i in range(size)]
 
     def root(self, index):
         while self.ids[index] != -1:
@@ -39,6 +15,34 @@ class Solution(object):
             self.ids[root_to] = root_from 
             return True
         return False
+
+
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        total = 0
+        union_count = 0
+        rows = len(grid)
+        if rows == 0: return 0
+        cols = len(grid[0])
+        if cols == 0: return 0
+        uf = UnionFind(cols * rows)
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == '1':
+                    total += 1
+                    cur_root = uf.root(i * cols + j)
+                    if j < cols - 1 and grid[i][j + 1] == '1':
+                        if uf.union(i * cols + j, i * cols + j + 1):
+                            union_count += 1
+                    if i < rows - 1 and grid[i + 1][j] == '1':
+                        if uf.union(i * cols + j, (i + 1) * cols + j):
+                            union_count += 1
+        return total - union_count
 
 
 sol = Solution()
